1b21f89994edfe41232ce648db5d5e8f9c76b2f3
Robert <rgitonga.07@gmail.com>
2022-06-06 18:15:35 +0300

    ft:add profile model

--- instagram/models.py
+++ instagram/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+class Profile(models.Model):
+    profile_image = CloudinaryField('image', null = True, default = 'default.jpg')
+    bio = models.TextField(default='')
+    user = models.OneToOneField(User, null = True, on_delete=models.CASCADE)
