3bfe829ae7678ef0f927f5768be300c701b22c7f
Robert <rgitonga.07@gmail.com>
2022-06-06 23:23:16 +0300

    ft: Add url pattern for searching

--- instagram/urls.py
+++ instagram/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('<int:post_id>/like',views.like, name = 'postlike'),
     path('comments/<int:pk>' , views.new_comment, name='comment'),
     path('new/' , views.new_post, name='new-post'),
+    path('search/', views.search_results, name ='search_results'),
 ]
