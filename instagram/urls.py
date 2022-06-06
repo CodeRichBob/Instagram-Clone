3129d0357fda49e6cea343e500d73b809facfbe9
Robert <rgitonga.07@gmail.com>
2022-06-06 22:50:08 +0300

    create url fol liking a post

--- instagram/urls.py
+++ instagram/urls.py
@@ -7,4 +7,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('' , views.post, name='post'),
     path('profile/' , views.profile, name='profile'),
+    path('<int:post_id>/like',views.like, name = 'postlike'),
 ]
