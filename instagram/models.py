b6c150871a20b07afbf42c269d5c82e7f7f6a681
Robert <rgitonga.07@gmail.com>
2022-06-06 18:24:14 +0300

    ft:add post model

--- instagram/models.py
+++ instagram/models.py
@@ -7,3 +7,14 @@ class Profile(models.Model):
     profile_image = CloudinaryField('image', null = True, default = 'default.jpg')
     bio = models.TextField(default='')
     user = models.OneToOneField(User, null = True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user.username}Profile'
+
+class Post (models.MOdel):
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    image = CloudinaryField('image', null=True)
+    caption = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    likes = models.IntegerField(default=0)
+    location = models.CharField(max_lenth=80, null=True)
